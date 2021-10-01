280ac5505d4f9e31e7554798f0aa6178b5f46fad
Guilherme Miranda <guilherme.miranda@jallcard.com.br>
2021-10-01 09:05:04 -0300

    Validação campo no campo nome

--- src/App.js
+++ src/App.js
@@ -12,11 +12,23 @@ class App extends Component{
         <Cabecalho/>
         <div className={"div"}>
             <Tabela/>
-            <FormularioCadastro/>
+            <FormularioCadastro aoEnviar={aoEnviarForm} validarNome={validarNome} />
         </div>
         </>
      );
    };
 };
+
+function aoEnviarForm(dados) {
+    console.log(dados);
+}
+
+function validarNome (nome){
+    if(nome.length < 3 || nome.length > 40) {
+        return {valido:false, texto: "O Nome teve entre 3 e 40 letras"};
+    }else{
+        return {valido:true, texto: ""};
+    }
+}
 
 export default App;
--- src/components/Formulario/FormularioCadastro.jsx
+++ src/components/Formulario/FormularioCadastro.jsx
@@ -1,71 +1,95 @@
-import React from "react";
-import {Button, Container, IconButton, Stack, TextField, Typography} from "@material-ui/core";
+import React, {useState} from "react";
+import {Button, IconButton, Stack, TextField} from "@material-ui/core";
 import {ReactComponent as DeleteIcon} from '../../images/remove.svg';
 import "./FormularioCadastro.css";
 
-function FormularioCadastro() {
+function FormularioCadastro({aoEnviar, validarNome, validarSobrenome}) {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [nascimento, setNascimento] = useState("");
+    const [parentesco, setParentesco] = useState("");
+    const [erros, setErros] = useState({nome:{valido: true, texto: ""}});
 
-    return(
-        // <Container component={"article"} maxWidth={"sm"} className={"container"}>
+    return (
         <div className={"container"}>
-            <form >
-                    <TextField
-                        id="nome"
-                        label="Nome"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth />
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    aoEnviar({nome,sobrenome, nascimento, parentesco});
+                }}
+            >
 
-                    <TextField
-                        id="sobrenome"
-                        label="Sobrenome"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth />
+                <TextField
+                    value={nome}
+                    onChange={(event) => {setNome(event.target.value)}}
+                    onBlur={(event) => {
+                        const ehValidoNome = validarNome(nome);
+                        setErros({nome:ehValidoNome})
+                    }}
+                    error={!erros.nome.valido}
+                    helperText={erros.nome.texto}
+                    id="nome"
+                    label="Nome"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth />
 
-                    <TextField
-                        id="dataNascimento"
-                        label="Data de Nascimento"
-                        type={"date"}
-                        margin="normal"
-                        required
-                        fullWidth
-                        InputLabelProps={{ shrink: true }}
-                        />
+                <TextField
+                    value={sobrenome}
+                    onChange={event => {setSobrenome(event.target.value)}}
 
-                    <TextField
-                        id="parentesco"
-                        label="Parentesco"
-                        margin="normal"
-                        required
-                        fullWidth
+                    id="sobrenome"
+                    label="Sobrenome"
+                    variant="outlined"
+                    margin="normal"
+                    // required
+                    fullWidth />
+
+                <TextField
+                    value={nascimento}
+                    onChange={event => {setNascimento(event.target.value)}}
+                    id="nascimento"
+                    label="Data de Nascimento"
+                    type={"date"}
+                    margin="normal"
+                    // required
+                    fullWidth
+                    InputLabelProps={{ shrink: true }}
                     />
-                    <Button
-                        variant="contained"
-                        fullWidth
-                    >Adicionar Número</Button>
-                    <div className={"campoNumero"}>
-                        <TextField
-                            id="numero"
-                            label="Número"
-                            type={"number"}
-                            margin="normal"
-                            required
 
-                        />
-                        <IconButton><DeleteIcon/></IconButton>
-                    </div>
-                    <Stack spacing={1} direction="row">
-                        <Button variant="contained" onClick={onsubmit} >Salvar</Button>
-                        <Button className={"buttons"} variant="outlin   ed" color="error" >Deletar</Button>
-                        <Button className={"buttons"} variant="outlined">Cancelar</Button>
-                    </Stack>
-                </form>
-            </div>
+                <TextField
+                    value={parentesco}
+                    onChange={event => {setParentesco(event.target.value)}}
+                    id="parentesco"
+                    label="Parentesco"
+                    margin="normal"
+                    // required
+                    fullWidth
+                />
+                <Button
+                    variant="contained"
+                    fullWidth
+                >Adicionar Número</Button>
+                <div className={"campoNumero"}>
+                    <TextField
+                        id="numero"
+                        label="Número"
+                        type={"number"}
+                        margin="normal"
+                        // required
+
+                    />
+                    <IconButton><DeleteIcon/></IconButton>
+                </div>
+                <Stack spacing={1} direction="row">
+                    <Button type={"submit"} variant="contained" onClick={onsubmit} >Salvar</Button>
+                    <Button className={"buttons"} variant="outlined" color="error" >Deletar</Button>
+                    <Button className={"buttons"} variant="outlined">Cancelar</Button>
+                </Stack>
+            </form>
+        </div>
     )
-
 }
 
 export default FormularioCadastro;
