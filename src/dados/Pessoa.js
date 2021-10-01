f99b1c8b8305507ecad64598eb2192e9dd2a01b6
Guilherme Miranda <guilherme.miranda@jallcard.com.br>
2021-10-01 16:06:44 -0300

    Criação da classe Pessoa e regras de validação de campos

---
+++ src/dados/Pessoa.js
@@ -0,0 +1,11 @@
+class Pessoa {
+    constructor(nome, sobrenome, dataNascimento, parentesco) {
+        this._nome = nome;
+        this._sobrenome = sobrenome;
+        this._dataNascimento = dataNascimento;
+        this._parentesco = parentesco;
+    }
+
+}
+
+export default Pessoa
---
+++ src/models/cadastro.js
@@ -0,0 +1,25 @@
+function validarNome (nome){
+    if(nome.length < 3 || nome.length > 40) {
+        return {valido:false, texto: "O Nome teve entre 3 e 40 letras"};
+    }else{
+        return {valido:true, texto: ""};
+    }
+}
+
+function validarSobrenome (sobrenome){
+    if(sobrenome.length < 3 || sobrenome.length > 50) {
+        return {valido:false, texto: "O Sobrenome teve entre 3 e 50 letras"};
+    }else{
+        return {valido:true, texto: ""};
+    }
+}
+
+function validarParentesco (parentesco){
+    if(parentesco.length < 2 || parentesco.length > 20) {
+        return {valido:false, texto: "O Parentesco teve entre 2 e 20 letras"};
+    }else{
+        return {valido:true, texto: ""};
+    }
+}
+
+export {validarNome, validarSobrenome, validarParentesco}
